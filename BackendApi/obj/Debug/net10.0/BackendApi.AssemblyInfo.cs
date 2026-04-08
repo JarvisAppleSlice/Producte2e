b208cd2780120bcdb22e0d17baedc6bd6d9d6297
JarvisAppleSlice <Samuel.s.jarvis@gmail.com>
2026-04-08 15:08:10 -0600

    first backend run

--- BackendApi/obj/Debug/net10.0/BackendApi.AssemblyInfo.cs
+++ BackendApi/obj/Debug/net10.0/BackendApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6263cdb939b3986ae926a8c0b6d416d5a505721f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f67c4798cc4eae806b5420f440d2cd7b6c6c550")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
