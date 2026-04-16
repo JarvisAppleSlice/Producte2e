e8861974941a51fbc9a1f62d8c9f0bb9da682483
JarvisAppleSlice <Samuel.s.jarvis@gmail.com>
2026-04-16 14:53:54 -0600

    Enable  account creation
    Added a registration form on a separate page.
    tests pass on cypress, error messages run, both email and password are required and show messages if not filled out properly.

---
+++ BackendApi/User.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    public string? PasswordHash { get; set; }
+}
